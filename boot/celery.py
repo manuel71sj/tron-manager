bbdf253d09adaa1f6f9d8535d99d3d2d87318d2a
한만철 <manuel71.sj@gmail.com>
2022-04-21 16:41:47 +0900

    celery monitoring 추가

--- boot/celery.py
+++ boot/celery.py
@@ -8,3 +8,8 @@ app = Celery('TRONManager')
 
 app.config_from_object('django.conf:settings', namespace='CELERY')
 app.autodiscover_tasks()
+
+'''
+celery multi start w1 -A boot.celery --loglevel=info -E
+celery --broker=amqp://tronmanager:dYvca5-sibcur-pydpec@129.154.59.231:5672/TronManager flower
+'''
