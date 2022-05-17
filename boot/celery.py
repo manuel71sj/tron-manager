ff435e065230b6f6df7dd3ca7c71e43ea75c4e0e
한만철 <manuel71.sj@gmail.com>
2022-05-17 12:34:13 +0900

    worker_cancel_long_running_tasks_on_connection_loss enable

--- boot/celery.py
+++ boot/celery.py
@@ -15,6 +15,7 @@ celery multi start w1 -A boot.celery --loglevel=info -E
 celery --broker=amqp://tronmanager:dYvca5-sibcur-pydpec@129.154.59.231:5672/TronManager flower
 '''
 
+app.conf.worker_cancel_long_running_tasks_on_connection_loss = True
 app.conf.update(
     CELERYBEAT_SCHEDULE={
         'check_wallet_status': {
